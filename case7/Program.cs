1eb97fd2bebd161a5917a52ddd9b31e2cddb527e
More2237 <noorallyrahmonov2308@gmail.com>
2024-11-29 15:08:58 +0500

    ihj

---
+++ case7/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int rakam = Int32.Parse(Console.ReadLine());
+        double vazn = Double.Parse(Console.ReadLine());
+
+        double resultkg = 0;
+        
+
+       
+    
+        
+
+        switch (rakam)
+        {
+            case 1: 
+                resultkg = vazn;
+                break;
+            case 2: 
+                resultkg = vazn* 0.000001;
+                break;
+            case 3: // Грамм
+                resultkg = vazn * 0.001;
+                break;
+            case 4: // Тонна
+                resultkg = vazn * 1000;
+                break;
+            case 5: // Центнер
+               resultkg = vazn* 100;
+                break;
+            default:
+                Console.WriteLine("incorrect num");
+                return;
+        }
+
+        Console.WriteLine(resultkg+" kg");
+    }
+}
